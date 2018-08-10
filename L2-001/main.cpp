e905d3989771039b0264f0855aaa3476ee449cbb
ke jun <2436199376@qq.com>
2018-08-11 00:34:36 +0800

    cccc-gplt

--- L2-001/main.cpp
+++ L2-001/main.cpp
@@ -37,12 +37,12 @@ int main()
     fill(dist,dist+v,MAX_LENGTH);
 
     for(int i = 0 ;i< v ;i++ )
-        cin>>man[i];
+        scanf("%d",&man[i]);
 
     for(int i = 0 ;i< e ;i++ )
     {
         int a,b,c;
-        cin>>a>>b>>c;
+        scanf("%d%d%d",&a,&b,&c);
         length[a][b] = length[b][a] = c;
         if(a == begin)
             path[b] = a;
@@ -91,91 +91,8 @@ int main()
             }
         }
     }
-
-    int index = end;
-
     cout<<num[end]<<" "<<nowMan[end]<<endl;
 
     printPath(end,begin,path);
 
-    //for(int i = vindex.size()-1 ;i>0  ;i-- )
-    //    cout<<vindex[i]<<" ";
-    //cout<<vindex[0]<<endl;
-
 }
-
-
-//#include <iostream>
-//#include <cstdio>
-//#include <algorithm>
-//using namespace std;
-//int n, m, c1, c2;
-//int dis[510], weight[510], e[510][510], num[510], w[510], pre[510];
-//bool visit[510];
-//const int inf = 99999999;
-//void printPath(int v) {
-//    if(v == c1) {
-//        printf("%d", v);
-//        return ;
-//    }
-//    printPath(pre[v]);
-//    printf(" %d", v);
-//}
-//int main() {
-//    scanf("%d%d%d%d", &n, &m, &c1, &c2);
-//    for(int i = 0; i < n; i++)
-//        scanf("%d", &weight[i]);
-//    fill(e[0], e[0] + 510 * 510, inf);
-//    fill(dis, dis + 510, inf);
-//    int a, b, c;
-//    for(int i = 0; i < m; i++) {
-//        scanf("%d%d%d", &a, &b, &c);
-//        e[a][b] = c;
-//        e[b][a] = c;
-//    }
-//    dis[c1] = 0;
-//    w[c1] = weight[c1];
-//    num[c1] = 1;
-//
-//    for(int i = 0 ;i< n ;i++ )
-//            cout<<w[i]<<" ";
-//        cout<<endl;
-//
-//    for(int i = 0; i < n; i++) {
-//        int u = -1, minn = inf;
-//        for(int j = 0; j < n; j++) {
-//            if(visit[j] == false && dis[j] < minn) {
-//                u = j;
-//                minn = dis[j];
-//            }
-//        }
-//        cout<<u<<endl;
-//        if(u == -1) break;
-//        visit[u] = true;
-//        for(int v = 0; v < n; v++) {
-//            if(visit[v] == false && e[u][v] != inf) {
-//                cout<<v<<" "<<e[u][v]<<" "<<dis[v]<<endl;
-//                if(dis[u] + e[u][v] < dis[v]) {
-//                    dis[v] = dis[u] + e[u][v];
-//                    num[v] = num[u];
-//                    w[v] = w[u] + weight[v];
-//                    cout<<v<<" "<<w[v]<<" "<<u<<endl;
-//                    pre[v] = u;
-//                } else if(dis[u] + e[u][v] == dis[v]) {
-//                    num[v] = num[v] + num[u];
-//                    if(w[u] + weight[v] > w[v]) {
-//                        w[v] = w[u] + weight[v];
-//                        pre[v] = u;
-//                    }
-//                }
-//            }
-//        }
-//        for(int i = 0 ;i< n ;i++ )
-//            cout<<w[i]<<" ";
-//        cout<<endl;
-//    }
-//    printf("%d %d\n", num[c2], w[c2]);
-//    printPath(c2);
-//    return 0;
-//}
-//
